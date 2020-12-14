c4927b0f2e75c6aaf9d3c82952bb28a643ca07a7
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-15 08:54:10 +0900

    :tada: init laravel

---
+++ docker-up.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 各種環境コンテナ立ち上げ
+# 別コンテナを立ち上げたい場合、各自ローカル環境でdocker-compose内を書き換えてください
+#
+# オプション:
+#   default :develop
+#   -l      :ローカル
+#   -s      :staging
+#   -p      :production
+#
+# 以下リストのコンテナを立ち上げたい場合、対応する環境のslot内に、コンテナ名を追記してください。
+#
+# デフォルトで立ち上げていないコンテナリスト
+#
+# 見えタロー接続(本番のみ、取扱注意)   : mysql-mietaro
+# develop環境redis(ローカルではない) : redis-dev
+
+#pushd ./laradock
+pushd ./docker
+
+ms='mysql'
+ro='read'
+
+while getopts 'bpsl' opts
+do
+    case $opts in
+        'b')
+            build='--build'
+            ;;
+#        'p')
+#            env='prod'
+#            slot="mysql-prod mysql-prod-read"
+#            ;;
+#        's')
+#            env='stg'
+#            slot='redis mysql-stg mysql-stg-read'
+#            ;;
+        'l')
+            env='local'
+#            slot='redis mysql'
+            slot='db db-testing'
+            ;;
+#        *)
+#            env='dev'
+#            slot="redis mysql-dev mysql-dev-read"
+#            ;;
+    esac
+done
+
+fixed_container="web"
+
+echo "Raunching container for ${env}..."
+eval "docker-compose up -d ${build} ${fixed_container} ${slot}"
+
+popd
